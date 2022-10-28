c0b6423564e18999011c6fa10693a70a7c5312b0
Mike Cowell <spike@spikeandbetty.com>
2022-10-28 13:56:43 -0400

    init commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        public BankAccount()
+        {
+        }
+        public void Deposit(double add)
+        {
+            balance += add;
+            Console.WriteLine($"you've added ${add} into your account for a new total balance of ${balance}");
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+    }
+
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World! Welcome to your Automatic ATM Machine!");           
+            var newAccount = new BankAccount();
+            bool isNumber;
+            double depo = 0.0;
+            do
+            {
+                Console.WriteLine("Please enter the amount of money you would like to desposit.");
+                isNumber = double.TryParse(Console.ReadLine(), out depo);
+            }
+            while (isNumber == false);
+
+            newAccount.Deposit(depo);
+            Console.WriteLine($"Your current account balance is ${newAccount.GetBalance()}");   
+
         }
     }
 }
